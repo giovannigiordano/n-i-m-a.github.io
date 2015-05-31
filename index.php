be3e6171927c25ca7a4ab2fba894cb8155454a01
Nima Amini <nima@digipars.com>
2015-05-31 17:23:41 +0200

    1st commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Nima Amini - IT Project Manager &amp; Consultant, PHP Developer &amp; SEO Specialist</title>
+    <meta name="description" content="I'm an IT Project Manager &amp; Consultant, PHP Developer &amp; SEO Specialist based in Milan (Italy), with a passion for solving problems.">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="./assets/main.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <header class="site-header container">
+      <div class="site-branding">
+        <h1 class="site-title"><a href="http://nima.pro/">Nima Amini - IT Project Manager &amp; Consultant</a></h1>
+      </div>
+    </header>
+    <div class="page-content">
+      <div class="wrapper">
+        <div class="bio container">
+          <p>I'm an IT Project Manager &amp; Consultant, PHP Developer &amp; SEO Specialist based in Milan <em>(Italy)</em>, with a passion for solving problems.</p>
+          <p>Over the past <?=(int)date('Y')-2002?> years, I have helped hundreds of satisfied clients &amp; companies to solve problems and to build technologies, targeting &amp; optimizing platforms with up-to 3 million happy users.</p>
+          <p>I am currently self-employed(/ a freelancer) and other than accepting the usual project-based work, am open to reasonable partnership/employment offers from Italian/Swiss-based companies &amp; startups.</p>
+          <p class="links">
+            <script type="text/javascript">
+            //<![CDATA[
+            <!--
+            var x="function f(x){var i,o=\"\",l=x.length;for(i=l-1;i>=0;i--) {try{o+=x.c" +
+            "harAt(i);}catch(e){}}return o;}f(\")\\\"function f(x,y){var i,o=\\\"\\\\\\\""+
+            "\\\\,l=x.length;for(i=0;i<l;i++){if(i==58)y+=i;y%=127;o+=String.fromCharCod" +
+            "e(x.charCodeAt(i)^(y++));}return o;}f(\\\"\\\\^T_HSZ.5l46,2\\\\\\\\\\\"\\\\" +
+            "$'bip,n'\\\\\\\\\\\"\\\\44n\\\\\\\\010w;615.4f0;\\\\\\\\037\\\\\\\\016\\\\\\"+
+            "\\010\\\\\\\\017\\\\\\\\002J\\\\\\\\025\\\\\\\\024\\\\\\\\0104KJ\\\\\\\\037" +
+            "\\\\\\\\005\\\\\\\\031\\\\\\\\002\\\\\\\\nM-P/\\\\\\\\r\\\\\\\\016\\\\\\\\r" +
+            "[\\\\\\\\023WYWDK\\\\\\\\004f\\\\\\\\031Z\\\\\\\\\\\\\\\\\\\\\\\\036Y!l'-2 " +
+            "*(8,\\\\\\\\026irqa&nm}2jw\\\\\\\\177lhb\\\"\\\\,58)\\\"(f};)lo,0(rtsbus.o " +
+            "nruter};)i(tArahc.x=+o{)--i;0=>i;1-l=i(rof}}{)e(hctac};l=+l;x=+x{yrt{)45=!)" +
+            "31/l(tAedoCrahc.x(elihw;lo=l,htgnel.x=lo,\\\"\\\"=o,i rav{)x(f noitcnuf\")"  ;
+            while(x=eval(x));
+            //-->
+            //]]>
+            </script>
+            <a href="https://it.linkedin.com/in/nimaamini" target="_blank"><i class="fa fa-linkedin"></i></a>
+            <a href="https://github.com/n-i-m-a" target="_blank"><i class="fa fa-github"></i></a>
+            <a href="https://instagram.com/nima_amini/" target="_blank"><i class="fa fa-instagram"></i></a>
+            <a href="https://twitter.com/nima_amini" target="_blank"><i class="fa fa-twitter"></i></a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <footer class="quote container">
+      <span class="line"></span>
+      <p>"Speech is silver and silence is golden."</p>
+    </footer>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-21255578-2', 'auto');
+  ga('send', 'pageview');
+  </script>
+  </body>
+</html>
